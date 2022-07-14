1c6bd97fd35a62f49c07c10f2deaa47d0039246a
Tin Le <thanhtin.le.vn@gmail.com>
2022-07-13 22:13:24 -0400

    User interface construction

---
+++ client/src/app/members/members-card/members-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-members-card',
+  templateUrl: './members-card.component.html',
+  styleUrls: ['./members-card.component.css']
+})
+export class MembersCardComponent implements OnInit {
+  @Input() member: Member;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
